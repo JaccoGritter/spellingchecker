c0fa4c8088f2a1c60afa4f1335cb8b77683d43d3
Jacco Gritter <jacco.gritter@gmail.com>
2019-05-31 16:59:27 +0200

    gebruik van jquery/ajax ipv formulier

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,15 +5,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <title>Spellingchecker</title>
+
+    <script>
+        $(document).ready(function() {
+
+
+            $("#myButton").click(function() {
+                // $("#resultaten").html( $("#woord").val() );
+                $.post("woordcheck.php", {
+                        woord: $("#woord").val()
+                    },
+                    function(data, status) {
+                        if (status == "success") {
+                            $("#resultaten").html(data);
+                        } else $("#resultaten").html("<b>connection failure</b>");
+                    });
+            });
+
+
+        });
+    </script>
+
 </head>
 
 <body>
-    <form action="woordcheck.php" method="POST">
-        <p>Voer een woord in om te checken</p>
-        <input type="text" name="woord">
-        <button value="submit">check!</button>
-    </form>
+    <p>Voer een woord in om te checken</p>
+    <input type="text" id="woord">
+    <button id="myButton">check!</button>
+    <br><br>
+    <div id="resultaten"></div>
 
 </body>
 
